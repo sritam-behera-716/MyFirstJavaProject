68b82ca70bb29709770b49d56b7423c7fbc15b14
sritam-behera-716 <sritambehera716@gmail.com>
2025-09-09 19:17:55 +0530

    Adding a new class student

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String name, rollNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+}
