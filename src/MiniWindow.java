187fcc3c8f97873a01fc2bd649a68ad5d9deb0e8
sritam-behera-716 <sritambehera716@gmail.com>
2025-09-10 06:14:58 +0530

    Adding the height & weight of the window

--- src/MiniWindow.java
+++ src/MiniWindow.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MiniWindow extends JFrame {
     MiniWindow() {
         JFrame frame = new JFrame("Mini Window");
+        frame.setSize(new Dimension(1280, 720));
         frame.setVisible(true);
     }
 
