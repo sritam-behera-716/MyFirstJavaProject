e6254301635e28ffd2168c0afe0b61acd7131365
sritam-behera-716 <sritambehera716@gmail.com>
2025-09-10 06:16:01 +0530

    Make the window to center of the screen

--- src/MiniWindow.java
+++ src/MiniWindow.java
@@ -5,6 +5,7 @@ public class MiniWindow extends JFrame {
     MiniWindow() {
         JFrame frame = new JFrame("Mini Window");
         frame.setSize(new Dimension(1280, 720));
+        frame.setLocationRelativeTo(frame);
         frame.setVisible(true);
     }
 
